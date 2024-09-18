1d4c95052307e7639c1158cea57c83825f1be50f
Jayesh Poduval <jayeshpoduval31@gmail.com>
2024-09-18 21:35:30 +1000

    Changed graph color and cleaned up code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import Graph from "./Graph";
+import { useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const duration = 60;
+  const startHour = 6;
+  const startMinutes = 30;
+
+  const newMinute = `${(startMinutes + time) % 60}`.padStart(2, "0");
+  const newHour = Math.floor(
+    startHour + Math.floor((startMinutes + time) / 60),
+  );
+  const timeFormat = `${newHour}:${newMinute}`;
+
   return (
     <section className="h-screen text-white">
       <div className="mx-auto flex h-full w-[90%] items-center justify-center">
         <div className="relative">
-          <Graph />
+          <Graph
+            timeFormat={timeFormat}
+            setTime={setTime}
+            duration={duration}
+          />
         </div>
       </div>
     </section>
